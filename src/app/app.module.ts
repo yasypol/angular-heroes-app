3d2a2b31a0af30020a80c573225216ac5801f35c
Yasira <yasypol@gmail.com>
2023-12-21 20:50:00 +0000

    Fin Sección 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
+
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,25 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { CardComponent } from './components/card/card.component';
 import { HeroesLayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
+import { MaterialModule } from '../material/material.module';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 
 @NgModule({
   declarations: [
-    HeroPageComponent,
+    CardComponent,
     HeroesLayoutPageComponent,
+    HeroPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+
+    HeroImagePipe
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class HeroesModule { }
